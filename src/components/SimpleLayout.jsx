dbb9debe3ec1dadbb8b24c99693aa15b21eecac2
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-12-12 12:27:43 +0100

    Add dummy data to article index page

--- src/components/SimpleLayout.jsx
+++ src/components/SimpleLayout.jsx
@@ -1,6 +1,6 @@
 import { Container } from '@/components/Container'
 
-export function SimpleLayout({ title, intro, children }) {
+export function SimpleLayout({ title, coverImage, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,32 +1,49 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { API_URL } from '@/config/index'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { getAllArticles } from '@/lib/getAllArticles'
+// import { getAllArticles } from '@/lib/getAllArticles'
 import { formatDate } from '@/lib/formatDate'
 
 function Article({ article }) {
   return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
-      <Card className="md:col-span-3">
-        <Card.Title href={`/articles/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Eyebrow
-          as="time"
-          dateTime={article.date}
-          className="md:hidden"
-          decorate
-        >
-          {formatDate(article.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
+    <article className="md:grid md:grid-cols-4 md:items-baseline lg:grid-cols-5">
+      <Card className="md:col-span-3 lg:col-span-4">
+        <div className="flex">
+          <div>
+            <Card.Title href={`/articles/${article.slug}`}>
+              {article.title}
+            </Card.Title>
+            <Card.Eyebrow
+              as="time"
+              dateTime={article.date}
+              className="md:hidden"
+              decorate
+            >
+              {formatDate(article.date)}
+            </Card.Eyebrow>
+            <Card.Description>{article.introduction}</Card.Description>
+            <Card.Cta>Read article</Card.Cta>
+          </div>
+          <div className="flex-shrink-0 sm:ml-4">
+            <div className="relative w-full h-40 overflow-hidden bg-white border-none rounded-md sm:w-52">
+              <Image
+                src={article.coverImage}
+                alt="Article image"
+                fill
+                className="object-cover"
+              ></Image>
+            </div>
+          </div>
+        </div>
       </Card>
       <Card.Eyebrow
         as="time"
         dateTime={article.date}
-        className="mt-1 hidden md:block"
+        className="hidden mt-1 md:block"
       >
         {formatDate(article.date)}
       </Card.Eyebrow>
@@ -49,7 +66,7 @@ export default function ArticlesIndex({ articles }) {
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-          <div className="flex max-w-3xl flex-col space-y-16">
+          <div className="flex flex-col max-w-3xl space-y-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
@@ -61,9 +78,11 @@ export default function ArticlesIndex({ articles }) {
 }
 
 export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/blogs`)
+  const articles = await res.json()
+
   return {
-    props: {
-      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
-    },
+    props: { articles },
+    revalidate: 1,
   }
 }
